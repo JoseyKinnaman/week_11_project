2af5ea0b6683b438bb2fdc2c6588d90e197178bc
Jozy Kinnaman <jozypants@gmail.com>
2020-05-29 13:50:12 -0700

    add vailidations for max and min content_body length. Dry up code.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,4 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
-  validates :name, presence: true
-  validates :cost, presence: true
-  validates :country_of_origin, presence: true
+  validates :name,:cost, :country_of_origin, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :product
-  validates :author, presence: true
-  validates :content_body, presence: true
-  validates :rating, presence: true
-  validates :content_body, length: { maximum: 250,
-    too_long: "%{count} characters is the maximum allowed" }
+  validates :author, :content_body, :rating, presence: true
+  validates :content_body, length: {  minimum: 50, maximum: 250 }
 end
