47de86f3c880e9caf5b731f50a218f62563d4f44
Jozy Kinnaman <jozypants@gmail.com>
2020-05-29 09:00:28 -0700

    add association between review and products

--- db/migrate/20200529151554_create_reviews.rb
+++ db/migrate/20200529151554_create_reviews.rb
@@ -4,8 +4,10 @@ class CreateReviews < ActiveRecord::Migration[5.2]
       t.column(:author, :string)
       t.column(:content_body, :string)
       t.column(:rating, :integer)
+     
 
       t.timestamps()
     end
   end
 end
+
