ba33a9b83d4bfcae13e389ab568af1289f5fb932
Jozy Kinnaman <jozypants@gmail.com>
2020-05-29 13:03:09 -0700

    add seeding for reviews.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
 Product.destroy_all
+Review.destroy_all
+
 
 50.times do |index|
-  Product.create!(name: Faker::Food.spice,
-                        cost: Faker::Number.between(from: 1, to: 10),
-                        country_of_origin: Faker::Address.country) 
+  product = Product.create!(name: Faker::Food.spice,cost: Faker::Number.between(from: 1, to: 10),country_of_origin: Faker::Address.country) 
+  product.save 
+      5.times do |review_index|
+      review = product.reviews.new(author: Faker::Name.name,content_body: Faker::Hipster.paragraph_by_chars(characters: 256, supplemental: false),
+      rating: Faker::Number.between(from: 1, to: 5), product_id: product.id) 
+      review.save                    
+    end
 end
 
-p "Created #{Product.count} products"
+p "Created #{Product.count} products and #{Review.count} reviews"
