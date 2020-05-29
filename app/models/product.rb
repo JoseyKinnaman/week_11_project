ef95ed2ce4ba3503aa70ca2fe132aeb83a2c02cb
Jozy Kinnaman <jozypants@gmail.com>
2020-05-29 13:29:04 -0700

    update test and add validation for Product input field verification.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, presence: true
+  validates :cost, presence: true
+  validates :country_of_origin, presence: true
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,5 +2,5 @@ require 'rails_helper'
 
 describe Product do
   it{ should have_many(:reviews)}
-  it { should validate presence_of :name, :cost, :country_of_origin}
+  it { should validate_presence_of :name, :cost, :country_of_origin}
 end
