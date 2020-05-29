4b6b61ace8b2d8f205cfb2f97075c6338bb16a19
Jozy Kinnaman <jozypants@gmail.com>
2020-05-29 15:52:50 -0700

    revert back to previous commit. Break in path for Update and new for Review. Speant last hour trouble shooting. Want to die.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,7 +6,7 @@ describe Product do
   it { should validate_presence_of :cost }
   it { should validate_presence_of :country_of_origin}
   it("titleizes the name of an product") do
-    product = product.create({name: "pastrami pickles", cost: 5, country_of_origin: "usa"})
+    product = Product.create({name: "pastrami pickles", cost: 5, country_of_origin: "usa"})
     expect(product.name()).to(eq("Pastrami Pickles"))
   end
 end
