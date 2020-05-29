b75602a9746931832bea6122f299505172efe6d8
Jozy Kinnaman <jozypants@gmail.com>
2020-05-29 13:42:39 -0700

    add validation for content_body Max. Expect to pass.

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,6 @@ class Review < ApplicationRecord
   validates :author, presence: true
   validates :content_body, presence: true
   validates :rating, presence: true
+  validates :content_body, length: { maximum: 250,
+    too_long: "%{count} characters is the maximum allowed" }
 end
