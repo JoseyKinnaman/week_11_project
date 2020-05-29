92c7b9622ec5d5915f7b44df392ba8deda9be680
Jozy Kinnaman <jozypants@gmail.com>
2020-05-29 11:08:27 -0700

    add seeding to products with Faker gem.

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'jquery-rails'
 gem 'bootstrap-sass', '~> 3.4.1'
 gem 'sassc-rails', '>= 2.1.0'
+gem 'faker'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+
+50.times do |index|
+  Product.create!(name: Faker::Food.spice,
+                        cost: Faker::Number.between(from: 1, to: 10),
+                        country_of_origin: Faker::Address.country) 
+end
+
+p "Created #{Product.count} products"
