97340d6271578da3e13323c5ce4cd30203a87d9a
Jozy Kinnaman <jozypants@gmail.com>
2020-05-29 10:17:04 -0700

    add bootstrap and initial css styling.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,30 +1,48 @@
 class ProductsController < ApplicationController
   def index
-    # Code for listing all albums goes here.
+    @products = Product.all 
+    render :index 
   end
 
   def new
-    # Code for new album form goes here.
+    @product = Product.new 
+
   end
 
   def create
-    # Code for creating a new album goes here.
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    end
   end
 
   def edit
-    # Code for edit album form goes here.
+    @product = Product.find(params[:id])
+    render :edit
   end
 
   def show
-    # Code for showing a single album goes here.
+    @product = Product.find(params[:id])
+    render :show
   end
 
   def update
-    # Code for updating an album goes here.
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
   end
 
   def destroy
-    # Code for deleting an album goes here.
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
   end
-  
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :cost, :country_of_origin)
+    end
 end
