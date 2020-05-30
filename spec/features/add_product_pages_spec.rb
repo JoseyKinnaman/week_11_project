8195f7f597763b9963680ddea2811aeaec73d246
Jozy Kinnaman <jozypants@gmail.com>
2020-05-29 17:32:30 -0700

    updated test for products_path. Test pass.

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -7,9 +7,9 @@ describe "the add a product process" do
     click_link 'Create new product'
     fill_in 'Name', :with => 'sticky rice'
     fill_in 'Cost', :with => '5'
-    fill_in 'Country of origin' :with => 'Thailand'
-    click_on 'Create product'
-    expect(page).to have_content 'product added to stock!'
+    fill_in 'Country of origin', :with => 'Thailand'
+    click_on 'Create Product'
+    expect(page).to have_content 'Product added to stock!'
     expect(page).to have_content 'Sticky Rice'
   end
 
