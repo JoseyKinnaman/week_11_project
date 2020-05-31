78e32858edb24a727871349f380964eef4466a82
Jozy Kinnaman <jozypants@gmail.com>
2020-05-30 19:38:21 -0700

    add test for adding a review. Test pass.

--- spec/features/add_review_pages_spec.rb
+++ spec/features/add_review_pages_spec.rb
@@ -2,15 +2,12 @@ require 'rails_helper'
 
 describe "the add a product process" do
   it "adds a new product" do
-    test_product = Product.new({name: "safron", cost: "7", country_of_origin: " spain"})
+    test_product = Product.new({name: "Safron", cost: 7, country_of_origin: "Spain"})
     test_product.save
     visit products_path
-    click_link "Safron"
-    click_link 'Add a review'
-    fill_in 'Author', :with => 'Fred Durst'
-    fill_in 'Review', :with => 'git'
-    fill_in 'Raiting', :with => '5'
-    click_on 'Create Review'
+    click_on "Safron"
+    click_on "Add a review"
+    click_on "Create Review"
     expect(page).to have_content 'Content body is too short (minimum is 50 characters)'
   end
 end
