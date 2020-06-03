9b17d1cd2950536cc45910d7f7d29d4572e262d2
Jozy Kinnaman <jozypants@gmail.com>
2020-06-03 08:43:59 -0700

    update seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ Review.destroy_all
 
 
 50.times do |index|
-  product = Product.create!(name: Faker::Food.spice,cost: Faker::Number.between(from: 1, to: 10),country_of_origin: Faker::Address.country) 
+  product = Product.create!(name: Faker::Food.spice,cost: Faker::Number.between(from: 2, to: 10),country_of_origin: Faker::Address.country) 
   product.save 
       5.times do |r_index|
       review = Review.create!(author: Faker::Name.name,content_body: Faker::Hipster.paragraph_by_chars(characters: 250, supplemental: false),
