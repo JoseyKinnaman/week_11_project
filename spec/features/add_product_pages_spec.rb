3218e1fa76bd529be901339829779a87dbb5b10c
Jozy Kinnaman <jozypants@gmail.com>
2020-06-05 14:49:52 -0700

    update testing to use warden. All tests passing.

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 
 describe "the add a product process" do
   it "adds a new product" do
+    user = User.create!(:email => 'test@example.com', :password => 'f4k3p455w0rd', :admin => true)
+    login_as(user, :scope => :user)
     visit products_path
     click_link 'Create new product'
     fill_in 'Name', :with => 'sticky rice'
@@ -14,6 +16,8 @@ describe "the add a product process" do
   end
 
   it "gives an error when no name is entered" do
+    user = User.create!(:email => 'test@example.com', :password => 'f4k3p455w0rd', :admin => true)
+    login_as(user, :scope => :user)
     visit new_product_path
     click_on 'Create Product'
     expect(page).to have_content "Name can't be blank"
--- spec/features/add_review_pages_spec.rb
+++ spec/features/add_review_pages_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 describe "the add a review process" do
   it "gives an error when no reviews content is entered" do
+    user = User.create!(:email => 'test@example.com', :password => 'f4k3p455w0rd', :admin => true)
+    login_as(user, :scope => :user)
     test_product = Product.new({name: "Safron", cost: 7, country_of_origin: "Spain"})
     test_product.save
     visit products_path
